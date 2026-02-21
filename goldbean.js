3ebed160386a79510f9885228a684107c124ed89
vanixcore <vani.j1926@gmail.com>
2026-02-21 22:06:36 +0530

    Add files via upload

---
+++ goldbean.js
@@ -0,0 +1,30 @@
+const shopName = "Gold Bean";
+console.log("Welcome to " + shopName); 
+const shopDescription = "A small shop that sells gold beans. They are said to be worth a lot of money, but no one has ever been able to prove it.";
+const shopItems = ["Gold Beans","Magic Beans","Dark Roast"];
+console.log(shopDescription);
+console.log("We have the following items for sale:");
+for (let i = 0; i < shopItems.length; i++) {
+    console.log("- " + shopItems[i]);
+}
+let coffeeProduct = {
+    name: "Gold Beans Espresso",
+    price: 15,
+    isAvailable: true
+};
+console.log("Our featured product is: " + coffeeProduct.name);
+console.log("Price: $" + coffeeProduct.price);
+if (coffeeProduct.isAvailable) {
+    console.log("This product is currently available.");
+} else {
+    console.log("Sorry, this product is currently out of stock.");}
+
+function calculateTotal(quantity) {
+    return quantity * coffeeProduct.price;
+}
+function displayOrder(quantity) {
+    let totalValue = calculateTotal(quantity);
+    console.log("Order: " + quantity + " bags of " + coffeeProduct.name);
+    console.log("Total Price: $" + totalValue);
+}
+
